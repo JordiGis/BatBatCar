4065accef4aa606a5cf674c3b0c50e8c589b52d5
JordiGis <jorgisfer@alu.edu.gva.es>
2024-05-10 23:13:50 +0200

    Initial commit

---
+++ src/main/java/es/progcipfpbatoi/BatBatCar.java
@@ -0,0 +1,18 @@
+package es.progcipfpbatoi;
+import es.progcipfpbatoi.menu.Menu;
+/**
+ * Clase principal.
+ * 
+ */
+
+
+
+public class BatBatCar {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.iniciar();
+        System.out.println();
+    }
+
+}
